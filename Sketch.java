bd05a952ebe8e9fce7c8e3a294e6d196674d8f3b
Ethan2108 <ethan.au23@ycdsbk12.ca>
2022-05-10 13:43:54 -0400

    snowballs falling (level 2)

--- Sketch.java
+++ Sketch.java
@@ -1,12 +1,31 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
+
+  float[] circleY = new float[30];
+  float[] circleX = new float[30];
+  boolean[] ballHideStatus = new boolean[30];
+
+  int intSnowballSpeed = 5;
+
+  float playerX = 350;
+  float playerY = 350;
+
+  boolean playerStatus = true;
+  boolean left;
+  boolean right;
+  boolean up;
+  boolean down;
+  int intLives = 3;
+
+  boolean mouseClicked = false;
+
 	 /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
     // put your size call here
-      size(400, 400);
+      size(700, 700);
     }
   
     /** 
@@ -14,21 +33,48 @@ public class Sketch extends PApplet {
      * values here i.e background, stroke, fill etc.
      */
     public void setup() {
-      background(210, 255, 173);
+      for (int i = 0; i < circleY.length; i++) {
+        circleY[i] = random(height);
+        circleX[i] = random(width);
+        ballHideStatus[i] = false;
+      }
     }
+    
   
     /**
      * Called repeatedly, anything drawn to the screen goes here
      */
     public void draw() {
+      if (playerStatus == true) {
+        background(0);
       
-    // sample code, delete this stuff
-      stroke(128);
-      line(150, 25, 270, 350);  
+        fill(255);
   
-      stroke(255);
-      line(50, 125, 70, 50);  
+        // draw snowballs to the screen if ballHideStatus is false
+        for (int i = 0; i < circleY.length; i++) {
+          if (ballHideStatus[i] == false) {
+            ellipse(circleX[i], circleY[i], 50, 50);
+  
+            circleY[i] += intSnowballSpeed;
+          }
+      
+          // resets snowball once it reaches end of screen
+          if (circleY[i] > height - 25) {
+            circleY[i] = 0;
+          }
+  
+          // if player circle collides with snowball, stop drawing snowball to screen and remove 1 life
+          if (dist(playerX, playerY, circleX[i], circleY[i]) <= 37.5 && ballHideStatus[i] == false) {
+            ballHideStatus[i] = true;
+            intLives--;
+          }
+          
+          // if snowball is clicked, stop drawing snowball to screen
+          if (dist(mouseX, mouseY, circleX[i], circleY[i]) <= 25 && mouseClicked) {
+              ballHideStatus[i] = true;
+          }
+          
+        }
     }
-    
-    // define other methods down here.
   }
+}
